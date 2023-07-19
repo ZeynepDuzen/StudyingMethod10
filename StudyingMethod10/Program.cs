e6375c1f17052348372f4057a0e189e526d89e36
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 08:49:58 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod10
+{
+    internal class Program
+    {
+        static int Sum(int x, int y)   // 3. method
+        {
+            return x + y;
+        }
+
+        //static int Sum(int x, int y) --> 2. method
+        //{
+        //    return x + y;
+        //}
+
+        //static int Sum(int x) --> 1. method
+        //{
+        //    return x + 3;
+        //}
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Sum(9)); --> 1.output
+
+            //Console.WriteLine(Sum(9,5)); --> 2. output
+
+            int z = Sum(9, 7);
+            Console.WriteLine(z);     //   --> 3. output
+
+            Console.Read();
+        }
+    }
+}
+
